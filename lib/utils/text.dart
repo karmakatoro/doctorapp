074b854c552edcb7724e37086ed9d80e0a067ec6
karmakatoro <karmakatoro@gmail.com>
2023-03-01 13:13:53 +0200

    text file finished

---
+++ lib/utils/text.dart
@@ -0,0 +1,14 @@
+// this is pre-define tesxt for app
+
+class AppText {
+  static final enText = {
+    'welcome_text': 'Welcome',
+    'signIn_text': 'Sign in to your account',
+    'registered_text': 'Already have an accont?',
+    'register_text':
+        'You can easily sign up, and connect to the Doctors nearby you',
+    'signUp_text': 'Dpn\'t have an account?',
+    'social-login': 'Or continue with social account',
+    'forgot-password': 'forget Your PassWwrd?'
+  };
+}
