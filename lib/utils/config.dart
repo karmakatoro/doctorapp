0d17deb2f448b5ca897e61d3976e3457f421d4fc
karmakatoro <karmakatoro@gmail.com>
2023-03-01 13:07:41 +0200

    config file finished

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -9,5 +9,36 @@ class Config {
   // width and height initialization
   void init(BuildContext context) {
     mediaQueryData = MediaQuery.of(context);
+    screenWidth = mediaQueryData!.size.width;
+    screenHeight = mediaQueryData!.size.height;
   }
+
+  static get widthSize {
+    return screenWidth;
+  }
+
+  static get heightSize {
+    return screenHeight;
+  }
+
+  // define spacing
+  static const spaceSmall = SizedBox(
+    height: 25,
+  );
+  static final spaceMedium = SizedBox(
+    height: screenHeight! * 0.05,
+  );
+  static final spaceBig = SizedBox(
+    height: screenHeight,
+  );
+
+  // textform field border
+  static const outlineBorder =
+      OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8)));
+
+  static const focusBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      borderSide: BorderSide(
+        color: Colors.red,
+      ));
 }
