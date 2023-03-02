61c7aaf38ef0f2d20b792783a1d09ef6b18622cc
karmakatoro <karmakatoro@gmail.com>
2023-03-02 19:48:44 +0200

    updates login page

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,4 +1,6 @@
+import 'package:doctorapp/utils/text.dart';
 import 'package:flutter/material.dart';
+import 'package:doctorapp/utils/config.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -10,8 +12,25 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
+    Config().init(context);
     return Scaffold(
-      body: Padding(),
+      body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 15,
+          ),
+          child: SafeArea(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                AppText.enText['welcome_text']!,
+                style:
+                    const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              )
+            ],
+          ))),
     );
   }
 }
