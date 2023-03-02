b6a419d3417ee16f7d5ed8f9ffc32fe0802fbcec
karmakatoro <karmakatoro@gmail.com>
2023-03-02 20:32:31 +0200

    error on obscured

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:doctorapp/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class LoginForm extends StatefulWidget {
@@ -8,10 +9,52 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  bool obscurePass = true;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Login Text Field'),
-    );
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        TextFormField(
+          controller: _emailController,
+          keyboardType: TextInputType.emailAddress,
+          cursorColor: Config.primaryColor,
+          decoration: const InputDecoration(
+              hintText: 'Email Address',
+              labelText: "Email",
+              alignLabelWithHint: true,
+              prefixIcon: Icon(Icons.email_outlined),
+              prefixIconColor: Config.primaryColor),
+        ),
+        Config.spaceSmall,
+        TextFormField(
+          controller: _passController,
+          keyboardType: TextInputType.visiblePassword,
+          cursorColor: Config.primaryColor,
+          obscureText: obscurePass,
+          decoration: const InputDecoration(
+              hintText: 'Your Password',
+              labelText: "Password",
+              alignLabelWithHint: true,
+              prefixIcon: const Icon(Icons.lock_outline),
+              prefixIconColor: Config.primaryColor,
+              suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: obscurePass
+                      ? const Icon(
+                          Icons.visibility_off_outlined,
+                          color: Colors.black,
+                        )
+                      : const Icon(
+                          Icons.visibility_outlined,
+                          color: Config.primaryColor,
+                        ))),
+        ),
+      ],
+    ));
   }
 }
